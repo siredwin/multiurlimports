34ec8fe711e6b8fffab96e11484f69c2325d7597
siredwin <edsiror@gmail.com>
2018-10-16 13:34:34 -0600

    changed some variable names

--- app2/routes.go
+++ app2/routes.go
@@ -8,8 +8,8 @@ func init(){
 	//Initiate the supercontext
 	e := supercontext.E
 	// Name the routes for app2
-	app1routes := e.Group("/app2")
-	app1routes.Match([]string{"GET", "POST"}, "/view3", view3)
-	app1routes.Match([]string{"GET", "POST"}, "/view4", view4)
+	app2routes := e.Group("/app2")
+	app2routes.Match([]string{"GET", "POST"}, "/view3", view3)
+	app2routes.Match([]string{"GET", "POST"}, "/view4", view4)
 
 }
